d8ee55172ccba22a4b2e7c2348270d3675686676
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-02-09 11:27:41 +0530

    Updates

--- Localhost_working_project/Files_for_Web_Server/Web_files/api_request.js
+++ Localhost_working_project/Files_for_Web_Server/Web_files/api_request.js
@@ -1,6 +1,11 @@
-function postData() {
+let check = document.getElementById('check_button_pincode') ;
+check.addEventListener('click' , () =>{
+    // console.log("Hello World");
+    let pin_value = document.getElementById('pincode_check_id_1').value ;
+    // console.log(pin_value) ;
+
     const apiUrl = 'http://localhost:8080/api/data';
-    const dataToSend = '5';
+    const dataToSend = pin_value;
 
     fetch(apiUrl, {
         method: 'POST',
@@ -16,9 +21,49 @@ function postData() {
         return response.text();
     })
     .then(data => {
-        console.log('Response:', data);
+        // console.log('Response:', data);
+        if(data != null){
+            let update_info = document.getElementById('deliverable_check_output_id') ;
+            update_info.innerHTML = `<p class="deliverable_check_output_para">Sellers Available for this Pincode</p>
+            <a href="http://www.google.co.in" id="seller_link" class="seller_details_button">View Sellers</a>` ;
+
+            console.log('Response:', data);
+        }
+        else{
+            let update_info = document.getElementById('deliverable_check_output_id') ;
+            update_info.innerHTML = `<p class="deliverable_check_output_para_2">Unfortunately no sellers are available for this pincode.</p>` ;
+            
+            console.log("Null Output") ;
+        }
     })
     .catch(error => {
         console.error('Error:', error);
     });
-}
+
+
+});
+
+// function postData() {
+//     const apiUrl = 'http://localhost:8080/api/data';
+//     const dataToSend = '5';
+
+//     fetch(apiUrl, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'text/plain'
+//         },
+//         body: dataToSend
+//     })
+//     .then(response => {
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         return response.text();
+//     })
+//     .then(data => {
+//         console.log('Response:', data);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
+// }
